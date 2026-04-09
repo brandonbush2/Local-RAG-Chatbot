903e0042ee50325d7eb4c6ba222e017a682dd37b
brandon <brandonabuga7@gmail.com>
2026-04-09 12:38:36 +0300

    Added trigger words for greetings, internet access and datetime

---
+++ utils.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+GREETING_TRIGGERS = [
+    "hi","hello","hey", "good morning", "good afternoon",
+    "good evening", "yoh", "what's up", "sup", "greetings"
+]
+
+INTERNET_TRIGGERS =[
+    "access the internet", "browse the web", "search online",
+    "go online", "real-time", "live data", "current news",
+    "latest news", "check online"
+]
+
+DATE_TRIGGERS - [
+    "today's date", "what date", "what day", "current date",
+    "what is today", "day is it", "when is today"
+]
